6f4e4b9917bd3be520b9c5d28353b043a0da3914
EGOIST <0x142857@gmail.com>
2016-09-30 17:45:32 +0800

    tweak

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-var base = require('./')
 
 module.exports = {
   extends: 'xo',
@@ -8,7 +7,7 @@ module.exports = {
     "semi": [2, "never"],
     "quote-props": ["error", "as-needed"],
     "curly": ["error", "multi-line", "consistent"],
-    "no-unused-vars": [2, { "vars": "all", "args": "none" }],
+    // "no-unused-vars": [2, { "vars": "all", "args": "none" }],
     "guard-for-in": 0,
     "no-warning-comments": 0,
     "new-cap": [2, { "newIsCap": true, "capIsNew": false }]
